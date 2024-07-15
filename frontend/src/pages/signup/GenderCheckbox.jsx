8d4f6bd35f9a3007d4f0602659e751753a23c694
Sakii <sakshiv2103@gmail.com>
2024-07-15 12:20:34 +0530

    Update GenderCheckbox.jsx

--- frontend/src/pages/signup/GenderCheckbox.jsx
+++ frontend/src/pages/signup/GenderCheckbox.jsx
@@ -3,7 +3,7 @@ const GenderCheckbox = ({ onCheckboxChange, selectedGender }) => {
 		<div className='flex'>
 			<div className='form-control'>
 				<label className={`label gap-2 cursor-pointer ${selectedGender === "male" ? "selected" : ""} `}>
-					<span className='label-text'>Male</span>
+					<span className='block text-white text-sm font-bold mb-2'>Male</span>
 					<input
 						type='checkbox'
 						className='checkbox border-slate-900'
@@ -14,7 +14,7 @@ const GenderCheckbox = ({ onCheckboxChange, selectedGender }) => {
 			</div>
 			<div className='form-control'>
 				<label className={`label gap-2 cursor-pointer  ${selectedGender === "female" ? "selected" : ""}`}>
-					<span className='label-text'>Female</span>
+					<span className='block text-white text-sm font-bold mb-2'>Female</span>
 					<input
 						type='checkbox'
 						className='checkbox border-slate-900'
